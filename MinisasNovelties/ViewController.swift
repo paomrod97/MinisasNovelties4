3f0ae62e9581d7deab7b73f7c35ee6ae3fef343a
Paola Rodriguez <paolam.rodriguezdurant@gmail.com>
2021-02-27 20:39:11 -0400

    Initial Commit

---
+++ MinisasNovelties/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MinisasNovelties
+//
+//  Created by Paola Rodriguez on 2/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
